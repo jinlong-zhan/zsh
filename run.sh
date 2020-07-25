1aef01db7875161dbf750737342dcd28fb89efd2
jinlong <hk53591013@gmail.com>
2020-07-25 15:21:50 +0800

    整合 setting-zshrc.sh 2l4

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker rm -vf zsh ; docker run -itd --name zsh zshusers/zsh ; docker cp install.sh zsh:/ ; docker cp setting-zshrc.sh zsh:/
+docker rm -vf zsh ; docker run -itd --name zsh zshusers/zsh ; docker cp install.sh zsh:/ 
